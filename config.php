f35fa765aa174d27a9e8c886a2d486ceaac84bd6
Tran Phu Quoc <spawnsuper@gmail.com>
2025-12-10 18:31:31 +0700

    push login

--- config.php
+++ config.php
@@ -2,7 +2,10 @@
 $host = 'localhost';
 $user = 'root';
 $pass = '';
-$db = '';
-
+$db = 'schema_database';
 
 $conn = mysqli_connect($host, $user, $pass, $db);
+if (!$conn) {
+    die("Kết nối thất bại: " . mysqli_connect_error());
+}
+?>

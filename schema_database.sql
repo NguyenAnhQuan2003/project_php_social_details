df86e0f04e75bce7a098daca5cbe284e093c4284
nguyenanhquan <quan715105194@gmail.com>
2025-12-14 10:41:09 +0700

    update db

--- schema_database.sql
+++ schema_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: localhost
--- Thời gian đã tạo: Th12 09, 2025 lúc 03:26 PM
+-- Thời gian đã tạo: Th12 14, 2025 lúc 04:39 AM
 -- Phiên bản máy phục vụ: 10.4.28-MariaDB
 -- Phiên bản PHP: 8.1.17
 
@@ -35,6 +35,15 @@ CREATE TABLE `comments` (
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Đang đổ dữ liệu cho bảng `comments`
+--
+
+INSERT INTO `comments` (`id`, `post_id`, `user_id`, `content`, `created_at`) VALUES
+(7, 10, 10, 'sadasdas', '2025-12-12 17:25:18'),
+(8, 11, 5, 'tét học liệu nhé', '2025-12-13 11:52:51'),
+(9, 10, 5, 'sadsadas', '2025-12-14 10:12:31');
+
 -- --------------------------------------------------------
 
 --
@@ -52,6 +61,18 @@ CREATE TABLE `posts` (
   `created_at` datetime DEFAULT current_timestamp(),
   `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `posts`
+--
+
+INSERT INTO `posts` (`id`, `user_id`, `title`, `content`, `type`, `video_url`, `status`, `created_at`, `updated_at`) VALUES
+(6, 10, 'MTP', 'Ca sĩ Việt Nam', 'text', NULL, 1, '2025-12-11 20:55:57', '2025-12-11 20:55:57'),
+(7, 10, 'dsadsa', 'dsadsadas', 'video', 'vid_1765462088_5722.mov', 1, '2025-12-11 21:08:08', '2025-12-11 21:08:08'),
+(8, 10, 'Đây là bài viết học tập', 'Hôm nay trời đẹp quá', 'text', NULL, 1, '2025-12-11 21:09:37', '2025-12-11 21:09:37'),
+(9, 10, 'sadasdas', 'sdadsadasdas', 'video', 'vid_1765463545_6242.mov', 1, '2025-12-11 21:32:25', '2025-12-11 21:32:25'),
+(10, 10, 'C++ Online', 'Học cùng Quân', 'video', 'vid_1765535062_4702.mov', 1, '2025-12-12 17:24:22', '2025-12-12 17:24:22'),
+(11, 5, 'Học nào', 'sadsadasdasdas', 'video', 'vid_1765601557_9301.mov', 1, '2025-12-13 11:52:37', '2025-12-13 11:52:37');
 
 -- --------------------------------------------------------
 
@@ -92,18 +113,24 @@ CREATE TABLE `users` (
   `role_id` int(11) DEFAULT NULL,
   `role_level` tinyint(4) DEFAULT 1,
   `created_at` datetime DEFAULT current_timestamp(),
-  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `status` varchar(50) DEFAULT 'active'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `role_id`, `role_level`, `created_at`, `updated_at`) VALUES
-(5, 'admin', 'admin@example.com', '1', 4, 4, '2025-12-09 20:55:23', '2025-12-09 20:55:23'),
-(6, 'mod', 'mod@example.com', '1', 3, 3, '2025-12-09 20:55:23', '2025-12-09 20:55:23'),
-(7, 'userA', 'userA@example.com', '1', 2, 2, '2025-12-09 20:55:23', '2025-12-09 20:55:23'),
-(8, 'viewA', 'viewA@example.com', '1', 1, 1, '2025-12-09 20:55:23', '2025-12-09 20:55:23');
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `role_id`, `role_level`, `created_at`, `updated_at`, `status`) VALUES
+(5, 'admin', 'admin@example.com', '1', 4, 4, '2025-12-09 20:55:23', '2025-12-12 17:38:36', 'active'),
+(6, 'mod', 'mod@example.com', '1', 3, 3, '2025-12-09 20:55:23', '2025-12-09 20:55:23', 'active'),
+(7, 'userA', 'userA@example.com', '1', 2, 2, '2025-12-09 20:55:23', '2025-12-09 20:55:23', 'active'),
+(8, 'viewA', 'viewA@example.com', '1', 1, 1, '2025-12-09 20:55:23', '2025-12-09 20:55:23', 'active'),
+(9, 'anhquan', 'quan@gmail.com', '1', 1, 1, '2025-12-10 21:50:27', '2025-12-10 21:50:27', 'active'),
+(10, 'quốc', 'aquan@gmail.com', '1', 1, 1, '2025-12-10 21:52:31', '2025-12-13 11:45:12', 'active'),
+(11, 'testsession', 'session@gmail.com', '1', 1, 1, '2025-12-10 21:55:14', '2025-12-10 21:55:14', 'active'),
+(12, 'anh', 'qa@gmail.com', '1', 1, 1, '2025-12-10 21:56:23', '2025-12-10 21:56:23', 'active'),
+(13, 'dump', 'dump@gmail.com', '1', 1, 1, '2025-12-10 22:14:01', '2025-12-10 22:14:01', 'active');
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -146,13 +173,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT cho bảng `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT cho bảng `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT cho bảng `roles`
@@ -164,7 +191,7 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT cho bảng `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
